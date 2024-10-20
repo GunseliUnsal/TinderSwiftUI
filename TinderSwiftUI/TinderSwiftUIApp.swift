d1efdca2dc3fe985d16613efea183be679a23b05
Gunseli Unsal <gunseliunsal@gmail.com>
2024-10-20 18:22:36 +0300

    Initial Commit

---
+++ TinderSwiftUI/TinderSwiftUIApp.swift
@@ -0,0 +1,17 @@
+//
+//  TinderSwiftUIApp.swift
+//  TinderSwiftUI
+//
+//  Created by Günseli Ünsal on 20.10.2024.
+//
+
+import SwiftUI
+
+@main
+struct TinderSwiftUIApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
